c6fbc0035004a61ac32ed704d118e200fc3ca509
speedy81523 <tylerjamestan@gmail.com>
2026-04-13 12:17:17 +0800

    Update MenuController.cs

--- Panda Unexpress/Assets/MainMenuScripts/MenuController.cs
+++ Panda Unexpress/Assets/MainMenuScripts/MenuController.cs
@@ -5,6 +5,8 @@ public class MenuController : MonoBehaviour
     public GameObject menu;
     public GameObject Settings;
     public GameObject Level;
+    public GameObject Controls;
+
 
     public void Start()
     {
@@ -33,9 +35,25 @@ public class MenuController : MonoBehaviour
         menu.SetActive(true);
         Settings.SetActive(false);
     }
+    public void OnClickControls()
+    {
+        Settings.SetActive(false);
+        Controls.SetActive(true);
+    }
 
+    public void OnClickControlsBack()
+    {
+        Settings.SetActive(true);
+        Controls.SetActive(false);
+    }
     public void Quit()
     {
-        //quit 
+
+        #if UNITY_EDITOR
+              UnityEditor.EditorApplication.isPlaying = false;
+        #else
+              Application.Quit();
+        #endif
+
     }
 }
