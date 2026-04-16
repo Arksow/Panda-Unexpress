c3314dc349796043b8b7e17233dda817d7c22986
Alvin Chan <alvinchan8303@gmail.com>
2026-04-16 15:23:01 +0800

    c

--- Panda Unexpress/Assets/Script/Counter.cs
+++ Panda Unexpress/Assets/Script/Counter.cs
@@ -1,22 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    [Header("Customer Reference")]
-    public CustomerAI customerManager;
+    private List<CustomerAI> customersAtCounter = new List<CustomerAI>();
 
     private void OnTriggerEnter(Collider other)
     {
+        CustomerAI customer = other.GetComponent<CustomerAI>();
+
+        if (customer != null)
+        {
+            if (!customersAtCounter.Contains(customer))
+                customersAtCounter.Add(customer);
+
+            return;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("Cup"))
         {
             CupData cup = other.GetComponent<CupData>();
 
-            if (cup != null && customerManager != null)
+            if (cup != null && customersAtCounter.Count > 0)
             {
-                customerManager.receivedOrder = cup;
-                customerManager.CheckOrder(cup);
+                CustomerAI targetCustomer = customersAtCounter[0];
+                targetCustomer.CheckOrder(cup);
+
                 Destroy(other.gameObject);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CustomerAI customer = other.GetComponent<CustomerAI>();
+
+        if (customer != null)
+        {
+            customersAtCounter.Remove(customer);
         }
     }
 }
