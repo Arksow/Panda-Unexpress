4c4cc3408e23aef4433d85abc86a3fcb5e4826fa
speedy81523 <tylerjamestan@gmail.com>
2026-04-17 09:44:08 +0800

    brightness etting

---
+++ Panda Unexpress/Assets/MainMenuScripts/BrightnessSlider.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BrightnessSlider : MonoBehaviour
+{
+    public Scrollbar BrightnessSlide;
+    public Image Brightness;
+
+    void Start()
+    {
+        float saved = PlayerPrefs.GetFloat("Brightness", 0f);
+        BrightnessSlide.value = saved;
+        SetBrightness(saved);
+
+        BrightnessSlide.onValueChanged.AddListener(SetBrightness);
+    }
+
+    public void SetBrightness(float value)
+    {
+        Color c = Brightness.color;
+        c.a = value;   //image set brightness overlay
+        Brightness.color = c;
+
+        PlayerPrefs.SetFloat("Brightness", value);
+    }
+}
+
