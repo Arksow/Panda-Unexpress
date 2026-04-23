ab9e2075aa4ff60edf977348384be11450363222
speedy81523 <tylerjamestan@gmail.com>
2026-04-23 17:50:19 +0800

    SZA

--- Panda Unexpress/Assets/Script/turtorialScript/IceScoopTask.cs
+++ Panda Unexpress/Assets/Script/turtorialScript/IceScoopTask.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class IceScoopTask : MonoBehaviour
 {
+    public TutorialManager manager;
+    public int currentTask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
